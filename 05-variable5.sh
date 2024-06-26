b7d5ffee940183302627de90f3471fed131edbbb
mundra venkatesh <mundra@venkatesh@gmail.com>
2024-06-26 17:40:24 +0530

    hello-world

---
+++ 05-variable5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter usernamer::"
+
+read username # here username is variable 
+
+echo "please enter password::"
+
+read password
+
+echo "username is : $ username ,password is :$ password"
