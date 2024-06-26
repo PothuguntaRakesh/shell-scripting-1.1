9eb4e681757381d2c98b9820570100de37b2c408
mundra venkatesh <mundra@venkatesh@gmail.com>
2024-06-26 18:11:44 +0530

    hello-world

---
+++ 06-variable6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+movies=("rrr","dj tillu", "lion")
+
+#size of above  array is 3
+
+# index are 0,1,2,
+
+# list always start from 0
+
+echo "first movie is $ {movies[0]}
+echo "first movie is $ {movies[1]}
+echo "first movie is $ {movies[@]}
