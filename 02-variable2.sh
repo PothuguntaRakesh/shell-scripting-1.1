cf5db0623e789eb33939829b58e9c3cd0f9712ba
mundra venkatesh <mundra@venkatesh@gmail.com>
2024-06-26 16:07:15 +0530

    hello-world

--- 02-variable2.sh
+++ 02-variable2.sh
@@ -1,2 +1,6 @@
-hi team 
-i searching on hold 
+#!/bin/bash
+
+echo "ramesh::hello suresh,how are you?"
+echo "suresh::hi ramesh, i am fine . how are you?"
+echo "ramesh::i am fine too, how is your work?"
+echo "suresh::not bad. i am thinking to upgrade go to devops"
