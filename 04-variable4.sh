adc88223ba940ec3664bfdaeb255a481d2b61c8e
mundra venkatesh <mundra@venkatesh@gmail.com>
2024-06-26 17:05:43 +0530

    hello-world

---
+++ 04-variable4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1::hello $person2,how are you?"
+echo "$person2::hi $person1, i am fine . how are you?"
+echo "$person1::i am fine too, how is your work?"
+echo "$person2::not bad. i am thinking to upgrade go to devops"
