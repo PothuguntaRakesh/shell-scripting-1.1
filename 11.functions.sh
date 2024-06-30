ee688b88660a9eac6b0431c96dd030ede3f2e6d2
mundra venkatesh <mundra@venkatesh@gmail.com>
2024-06-30 10:56:05 +0530

    some thing more

---
+++ 11.functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(idn-u)
+
+if [$n USERID -ne 0]
+
+then 
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes 
+
+    else
+        echo "you are super user."
+    
+    if 
+
+      dnf install mysql -y
+
+      echo "is script proceeding?"
+
+
